6cbca89e3a4e354fc2a76e044578b2e86fdbaa34
Qzt-1007 <13089880013@163.com>
2026-04-26 21:04:55 +0800

    1

--- include/VisStress.h
+++ include/VisStress.h
@@ -46,7 +46,55 @@ Array2D Tau_xx(Array2D u, Array2D v, Array2D Vis, int indi)  // indi=0--前向�
 
     for (int i = 0; i < IMAX; i++) {
         for (int j = 0; j < JMAX; j++) {
-            Tau[i][j] = (4.0 / 3.0)*Vis[i][j]*dudx[i][j] - (2.0/3.0)*Vis[i][j]*dvdy[i][j];
+            Tau[i][j] = (4.0 / 3.0) * Vis[i][j] * dudx[i][j] - (2.0 / 3.0) * Vis[i][j] * dvdy[i][j];
+        }
+    }
+
+    return Tau;
+}
+
+Array2D Tau_yy(Array2D u, Array2D v, Array2D Vis, int indi)  // indi=0--前向差分,indi=1--后向差分
+{
+    Array2D Tau, dudx, dvdy = createArray2D(IMAX, JMAX, 0.0);
+    for (int i = 0; i < IMAX; i++) {
+        for (int j = 0; j < JMAX; j++) {
+            if (j == 0) {
+                dudx[i][j] = (-3 * u[i][j] + 4 * u[i][j + 1] - u[i][j + 2]) / (2 * Dx);
+            } else if (j == JMAX - 1) {
+                dudx[i][j] = (-3 * u[i][j] + 4 * u[i][j - 1] - u[i][j - 2]) / (2 * Dx);
+            } else {
+                dudx[i][j] = (u[i][j + 1] - u[i][j - 1]) / (2 * Dx);
+            }
+        }
+    }
+
+    if (indi == 0) {  // 前向差分
+        for (int i = 0; i < IMAX; i++) {
+            for (int j = 0; j < JMAX; j++) {
+                if (i == IMAX - 1) {
+                    dvdy[i][j] = (-3 * v[i][j] + 4 * v[i - 1][j] - v[i - 2][j]) / (2 * Dy);
+                } else {
+                    dvdy[i][j] = (v[i + 1][j] - v[i][j]) / Dx;
+                }
+            }
+        }
+    } else if (indi == 1) {  // 后向差分
+        for (int i = 0; i < IMAX; i++) {
+            for (int j = 0; j < JMAX; j++) {
+                if (i == 0) {
+                    dvdy[i][j] = (-3 * v[i][j] + 4 * v[i + 1][j] - v[i + 2][j]) / (2 * Dy);
+                } else {
+                    dvdy[i][j] = (v[i][j] - v[i - 1][j]) / Dy;
+                }
+            }
+        }
+    } else {
+        throw std::runtime_error("indi should only be 0 or 1.");
+    }
+
+    for (int i = 0; i < IMAX; i++) {
+        for (int j = 0; j < JMAX; j++) {
+            Tau[i][j] = (4.0 / 3.0) * Vis[i][j] * dvdy[i][j] - (2.0 / 3.0) * Vis[i][j] * dudx[i][j];
         }
     }
 
