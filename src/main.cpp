9e76a908a093ac92dae4cc32eb592246170951aa
Qzt-1007 <13089880013@163.com>
2026-04-26 16:46:10 +0800

    1

--- src/main.cpp
+++ src/main.cpp
@@ -27,7 +27,14 @@ constexpr double k0 = DynVis0 * Cp / Pr;
 // 2D 数组类型别名
 using Array2D = std::vector<std::vector<double>>;
 
+// 3D 数组类型别名 (x, y, component)
+using Array3D = std::vector<std::vector<std::vector<double>>>;
+
 Array2D createArray2D(int nx, int ny, double initVal = 0.0) { return Array2D(nx, std::vector<double>(ny, initVal)); }
+
+Array3D createArray3D(int nx, int ny, int ncomp, double initVal = 0.0) {
+    return Array3D(nx, std::vector<std::vector<double>>(ny, std::vector<double>(ncomp, initVal)));
+}
 
 int main() {
     const double Dd = 5 * LHORI / ReL;
