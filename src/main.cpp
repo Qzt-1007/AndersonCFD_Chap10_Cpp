1a89202bf325349f7ff3a4a7d65982566377dd16
Qzt-1007 <13089880013@163.com>
2026-04-26 16:53:48 +0800

    1

--- src/main.cpp
+++ src/main.cpp
@@ -42,15 +42,23 @@ int main() {
     const double Dx = LHORI / (IMAX - 1);
     const double Dy = LVERT / (JMAX - 1);
 
-    Array3D Q = createArray3D(IMAX, JMAX, 4, 0.0);
+    Array3D Q = createArray3D(IMAX, JMAX, 4, 0.0);  // u,v,p,T
 
     // 初值条件
     for (int ii = 0; ii < IMAX; ii++) {
         for (int jj = 0; jj < JMAX; jj++) {
             Q[ii][jj][0] = Ma0 * a0;  // u
-            Q[ii][jj][1] = 0.0; //v
-            Q[ii][jj][2] = p0; //p
-            Q[ii][jj][3] = T0; //T
+            Q[ii][jj][1] = 0.0;       // v
+            Q[ii][jj][2] = p0;        // p
+            Q[ii][jj][3] = T0;        // T
         }
+    }
+
+    // 壁面初值条件
+    for (int ii = 0; ii < IMAX; ii++) {
+        Q[ii][0][0] = 0;
+        Q[ii][0][1] = 0;
+        Q[ii][0][2] = p0;
+        Q[ii][0][3] = Tw;
     }
 }
